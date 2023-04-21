569ffa768a2d549582307878b11f58dfc6de7a55
tanlooger <tanlooger@gmail.com>
2023-04-21 19:41:57 +0800

    3

--- books/yi/files/chapters.js
+++ books/yi/files/chapters.js
@@ -4,8 +4,8 @@ const fs = require('fs');
 let bookdata = fs.readFileSync('../40/a.txt', 'utf8')
 
 let reg = /　/g
-reg = /\n((第.部分.*)|(第[0-9]{1,2}章 .*)|([0-9]{1,2}))\n/g
 reg = /\n((第.部分.*)|(第[一二三四五六七八九十]{1,3}章.*)|([0-9]{1,2})|([一二三四五六七八九十]、.*))\n/g
+reg = /\n((第.部分.*)|(第[0-9]{1,2}章 .*)|([0-9]{1,2}))\n/g
 
 const chaps = bookdata.match(reg)
 
