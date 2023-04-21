e3c089b5d3436951aa2ecc96f75fb9361d9f3fd4
tanlooger <tanlooger@gmail.com>
2023-04-21 09:09:03 +0800

    帝国的年代：1875—1914-贾士蘅译

--- books/yi/files/chapters.js
+++ books/yi/files/chapters.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
 
-let bookdata = fs.readFileSync('../27/a.txt', 'utf8')
+let bookdata = fs.readFileSync('../28/a.txt', 'utf8')
 
 const reg = /\n((第.部分.*)|(第[一二三四五六七八九十]{1,3}章.*)|([0-9]{1,2}))\n/g
 const chaps = bookdata.match(reg)
--- books/yi/files/splitter.js
+++ books/yi/files/splitter.js
@@ -3,7 +3,7 @@ const { assert } = require("console");
 const fs = require("fs");
 const path = require("path");
 
-const bookid = 27;
+const bookid = 28;
 const relevels = [3]
 let bookdata = fs.readFileSync(`../${bookid}/a.txt`, "utf8");
 
