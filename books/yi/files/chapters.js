ca2eea2df849021b5e71634f81d57c3b0c75b000
tanlooger <tanlooger@gmail.com>
2023-04-21 22:25:03 +0800

    文明史：人类五千年文明的传承与交流-常绍民等译

--- books/yi/files/chapters.js
+++ books/yi/files/chapters.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
 
-let bookdata = fs.readFileSync('../42/a.txt', 'utf8')
+let bookdata = fs.readFileSync('../45/a.txt', 'utf8')
 
 let reg = /　/g
 // \n\n\n\n\n\n.+(?<![。？\.\-])\n
@@ -9,6 +9,7 @@ reg = /\n((第.部分.*)|(第[一二三四五六七八九十]{1,3}章.*)|([0-9]{
 reg = /\n((第.部分.*)|(第[0-9]{1,2}章 .*)|([0-9]{1,2}))\n/g
 reg = /\n((第.部 .*)|(第[0-9]{1,2}章 .*)|(\n\n\n\n\n.+(?<![。？\.\-])))\n/g
 reg = /\n((第.部 .*)|(第[0-9]{1,2}章 .*))\n/g
+reg = /\n(([上中下]编 .*)|(第.部分 .*)|(第[一二三四五六七八九十]{1,3}章 .*))\n/g
 
 const chaps = bookdata.match(reg)
 
