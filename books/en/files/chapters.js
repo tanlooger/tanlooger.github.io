d4a3ae860b8abd4f4dc339b3e5911ef7bd19c613
tanlooger <tanlooger@gmail.com>
2023-05-03 18:50:27 +0800

    The Pearl by John Steinbeck

--- books/en/files/chapters.js
+++ books/en/files/chapters.js
@@ -1,11 +1,11 @@
 const fs = require('fs');
 
 
-let bookdata = fs.readFileSync('../34/a.txt', 'utf8')
+let bookdata = fs.readFileSync('../39/a.txt', 'utf8')
 
 let reg = /　/g
 // \n\n\n\n\n\n.+(?<![。？\.\-])\n
-reg = /\n((PART (ONE|TWO|THREE))|(ONE|TWO|THREE|FOUR|FIVE|SIX|SEVEN|EIGHT|NINE|TEN)|(第[一二三四五六七八九十]{1,3}章 .*))\n/g
+reg = /\n((PART (ONE|TWO|THREE))|(ONE|TWO|THREE|FOUR|FIVE|SIX|SEVEN|EIGHT|NINE|TEN)|(第[一二三四五六七八九十]{1,3}章 .*)|(I|II|III|IV|V|VI|VII))\n/g
 
 const chaps = bookdata.match(reg)
 
