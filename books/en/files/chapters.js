446069068ec5ff59f536deb87352a93e485e7791
tanlooger <tanlooger@gmail.com>
2023-05-14 11:42:14 +0800

    The Hunt for Red October

--- books/en/files/chapters.js
+++ books/en/files/chapters.js
@@ -1,16 +1,17 @@
 const fs = require('fs');
 
 
-let bookdata = fs.readFileSync('../41/a.txt', 'utf8')
+let bookdata = fs.readFileSync('../42/a.txt', 'utf8')
 
 let reg = /　/g
 // \n\n\n\n\n\n.+(?<![。？\.\-])\n
 reg = /\n((PART (ONE|TWO|THREE))|(ONE|TWO|THREE|FOUR|FIVE|SIX|SEVEN|EIGHT|NINE|TEN)|(第[一二三四五六七八九十]{1,3}章 .*)|(I|II|III|IV|V|VI|VII))|((BOOK (ONE|TWO|THREE)))|(Chapter [0-9]{1,2})\n/g
 reg = /\n((BOOK (ONE|TWO|THREE)))|(Chapter [0-9]{1,2})\n/g
 reg = /\n(Chapter [0-9]) - .*\n/g
+reg = /\nTHE .* DAY：.*\n/g
 
 const chaps = bookdata.match(reg)
 
-chaps.map(v=>console.log(v.trim()))
+chaps.map(v=>console.log('　'+v.trim()))
 
 
