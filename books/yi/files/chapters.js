7a4367157d6faf18a2182810eba0bc0c085c73d2
tanlooger <tanlooger@gmail.com>
2023-05-30 12:27:52 +0800

    资本论 - 郭大力、王亚南译

--- books/yi/files/chapters.js
+++ books/yi/files/chapters.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
 
-let bookdata = fs.readFileSync('../63/a.txt', 'utf8')
+let bookdata = fs.readFileSync('../65/a.txt', 'utf8')
 
 let reg = /　/g
 // \n\n\n\n\n\n.+(?<![。？\.\-])\n
@@ -13,9 +13,10 @@ reg = /\n((第.部分 .*)|(第[0-9]{1,2}章 .*)|([0-9]{1,2}))\n/g
 reg = /\n(([上中下]编 .*)|(第.部分 .*)|(第[一二三四五六七八九十]{1,3}章 .*))\n/g
 reg = /\n(([上中下]册 .*)|(第[一二三四五六七八九十]{1,3}[章节] .*))\n/g
 reg = /\n(.{3,50})\n/g
+reg = /\n((资本论- 第.部)|[IVXL]{1,5}[、．].*|[0-9]{1,5}、.*|(第[一二三四五六七八九十]{1,3}章.*))\n/g
 
 const chaps = bookdata.match(reg)
 
-chaps.map(v=>console.log(v.trim()))
+chaps.map(v=>console.log('　　　'+v.trim()))
 
 
