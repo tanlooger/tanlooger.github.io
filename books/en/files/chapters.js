b3f7778384c3c7c83a1c787dedca313407cda7f6
tanlooger <tanlooger@gmail.com>
2023-06-06 11:57:05 +0800

    Dune by Frank Herbert

--- books/en/files/chapters.js
+++ books/en/files/chapters.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
 
-let bookdata = fs.readFileSync('../47/a.txt', 'utf8')
+let bookdata = fs.readFileSync('../50/a.txt', 'utf8')
 
 let reg = /　/g
 // \n\n\n\n\n\n.+(?<![。？\.\-])\n
@@ -10,10 +10,11 @@ reg = /\n((BOOK (ONE|TWO|THREE)))|(Chapter [0-9]{1,2})\n/g
 reg = /\n(Chapter [0-9]) - .*\n/g
 reg = /\nTHE .* DAY：.*\n/g
 reg = /\n((Book (One|Two|Three|Four|Five|Six|Seven|Eight|Nine|Ten|Eleven)))|([0-9]{1,3})\n/g
+reg = /\n((BOOK (ONE|TWO|THREE) .*)|([0-9]{1,2})|APPENDIXES)\n/g
 
 
 const chaps = bookdata.match(reg)
 
-chaps.map(v=>console.log('　'+v.trim()))
+chaps.map(v=>console.log('　　'+v.trim()))
 
 
