e2a056268ccc5a5351ae56c5bda9a5d11cd88e3b
tanlooger <tanlooger@gmail.com>
2023-06-12 15:47:24 +0800

    Magic Tree House

--- books/en/files/make.js
+++ books/en/files/make.js
@@ -19,65 +19,65 @@ const arr = [
   const CHAPTER = 3
 
   //const data = fs.readFileSync('../33/a.txt', 'utf8');
-  const data = fs.readFileSync('a/'+CHAPTER+'.txt', 'utf8');
+  //const data = fs.readFileSync('a/'+CHAPTER+'.txt', 'utf8');
 
  //const re = /(第[一二三四五六七八九十]{1,3}章)/g
  //const re = '\n\n([一二三四五六七八九十]{1,2})\n\n'
  //const re = /(● .*\n\n)/g
  //const re = /(【.*】)/g
- let re = '\n\n('+arr.join('|').trim()+')\n\n'
- re = '\n\n\n\n(.*)\n\n\n\n\n'
- //re = '\n\n\n(CHAPTER .*)\n'
- re = '\n\n\n([0-9]{1,2}. .*)\n\n\n\n'
- re = '\n([0-9]{1,2})\n'
- re = '\n\n(Chapter [0-9]{1,2})\n\n'
- re = '\n\n(CHAPTER [0-9]{1,2} .*)\n\n'
- re = '\n(CHAPTER .*)\n\n'
- re = '\n\n\n\n((X|I|V){1,5}\..*)\n\n'
- re = '\n\\[ ([0-9]{1,3}) \\]\n'
- re = '\n(Chapter [0-9] .*)\n'
- re = '\n\n('+arr.join('|').trim()+')\n\n'
- re = '\n(Chapter [0-9]{1,2} .*)\n'
- re = '\n(Book the .*)\n'
- re = '\n(CHAPTER (X|I|V){1,5}. .*)\n'
+//  let re = '\n\n('+arr.join('|').trim()+')\n\n'
+//  re = '\n\n\n\n(.*)\n\n\n\n\n'
+//  //re = '\n\n\n(CHAPTER .*)\n'
+//  re = '\n\n\n([0-9]{1,2}. .*)\n\n\n\n'
+//  re = '\n([0-9]{1,2})\n'
+//  re = '\n\n(Chapter [0-9]{1,2})\n\n'
+//  re = '\n\n(CHAPTER [0-9]{1,2} .*)\n\n'
+//  re = '\n(CHAPTER .*)\n\n'
+//  re = '\n\n\n\n((X|I|V){1,5}\..*)\n\n'
+//  re = '\n\\[ ([0-9]{1,3}) \\]\n'
+//  re = '\n(Chapter [0-9] .*)\n'
+//  re = '\n\n('+arr.join('|').trim()+')\n\n'
+//  re = '\n(Chapter [0-9]{1,2} .*)\n'
+//  re = '\n(Book the .*)\n'
+//  re = '\n(CHAPTER (X|I|V){1,5}. .*)\n'
 
 
- const reg = new RegExp(re, 'g')
- console.log(reg)
+//  const reg = new RegExp(re, 'g')
+//  console.log(reg)
 
- const data2 = data.replace(reg, 'asdffsdafasdsf$1adfdsdeo97j')
+//  const data2 = data.replace(reg, 'asdffsdafasdsf$1adfdsdeo97j')
 
- const myArray = data2.split('asdffsdafasdsf');
+//  const myArray = data2.split('asdffsdafasdsf');
 
- let json = []
+//  let json = []
 
 
 
-console.log('adsfsd'+myArray.length)
+//console.log('adsfsd'+myArray.length)
 
-//for(let i=21; i<=50; i++){
-  for(let i=1; i<=myArray.length; i++){
+for(let i=51; i<=100; i++){
+ // for(let i=1; i<=myArray.length; i++){
     //console.log(i)
-  const chapter = myArray[i].split("adfdsdeo97j")
+  //const chapter = myArray[i].split("adfdsdeo97j")
 
   //console.log(chapter[0])
 
-  json.push({
-    //"slug": "en/33/"+i,
-    "slug": "en/33/"+CHAPTER+'/'+i,
-    //"title": arr[i],
-    "title": chapter[0],
-    //"child":[]
-  })
+  // json.push({
+  //   //"slug": "en/33/"+i,
+  //   "slug": "en/33/"+CHAPTER+'/'+i,
+  //   //"title": arr[i],
+  //   "title": chapter[0],
+  //   //"child":[]
+  // })
 
   //fs.writeFileSync(i+'.mdx', "---\n---\n---\ntitle: 睡前消息"+i+"期：\n---\n\n日睡前消息文字版第"+i+"期");
-  fs.writeFileSync(i+'.mdx', "---\n---\n---\ntitle: "+chapter[0].replace('●','').trim()+"\n---\n\n"+chapter[1].trim());
+  //fs.writeFileSync(i+'.mdx', "---\n---\n---\ntitle: "+chapter[0].replace('●','').trim()+"\n---\n\n"+chapter[1].trim());
   //fs.writeFileSync(i+'.mdx', "---\n---\n---\ntitle: "+arr[i].trim()+"\n---\n\n");
   //fs.writeFileSync('a/'+i+'.txt', chapter[0]+ '\n\n\n\n\n\n'+ chapter[1].trim());
-  fs.writeFileSync('bookchap.json', JSON.stringify(json));
+  //fs.writeFileSync('bookchap.json', JSON.stringify(json));
 
   //fs.writeFileSync(i+'.mdx', "---\n---\n---\ntitle: "+chapter[0]+"\n---\n\n");
-  //fs.writeFileSync(i+'.mdx', "---\n---\n---\ntitle: \n---\n\n");
+  fs.writeFileSync(i+'.mdx', "---\n---\n---\ntitle: \n---\n\n");
 
 }
 
