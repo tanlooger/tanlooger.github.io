b87f0deff10e1e4f4aa7f8b6f1aed32bc4588bcc
tanlooger <tanlooger@gmail.com>
2023-07-04 06:25:12 +0800

    天下才子必读书 - 金圣叹选批

---
+++ books/cn/files/superscript.js
@@ -0,0 +1,39 @@
+
+
+var fs = require('fs')
+const path = require('path');
+
+const ss = ['⁰','¹','²','³','⁴','⁵','⁶','⁷','⁸','⁹']
+
+// find superscript
+// \p{No}
+
+const filename = '../133/a.txt'
+
+    fs.readFile(filename, 'utf8', function (err,data) {
+        if (err) {
+          return console.log(err);
+        }
+
+        var result
+
+        result = data.replace(/【(\d)(\d)?(\d)?】/g, ($1, $2, $3, $4, $5)=>{
+          console.log($3)
+          
+          
+          if($5 < 10)return ss[$2]+ss[$3]+ss[$4]+ss[$5]
+          if($4 < 10)return ss[$2]+ss[$3]+ss[$4]
+          if($3 < 10)return ss[$2]+ss[$3]
+          if($2 < 10)return ss[$2]
+          return $1
+        })
+
+        console.log(RegExp.$1)
+
+        fs.writeFile('aa.txt', result, 'utf8', function (err) {
+           if (err) return console.log(err);
+        });
+      });
+
+
+
