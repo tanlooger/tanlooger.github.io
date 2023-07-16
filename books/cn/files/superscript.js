2a6307c2234f077810a339f2168a1ee11a623857
tanlooger <tanlooger@gmail.com>
2023-07-16 18:56:37 +0800

    古文观止

--- books/cn/files/superscript.js
+++ books/cn/files/superscript.js
@@ -8,7 +8,7 @@ const ss = ['⁰','¹','²','³','⁴','⁵','⁶','⁷','⁸','⁹']
 // find superscript
 // \p{No}
 
-const filename = '../133/a.txt'
+const filename = '../47/a.txt'
 
     fs.readFile(filename, 'utf8', function (err,data) {
         if (err) {
@@ -17,7 +17,7 @@ const filename = '../133/a.txt'
 
         var result
 
-        result = data.replace(/【(\d)(\d)?(\d)?】/g, ($1, $2, $3, $4, $5)=>{
+        result = data.replace(/\[(\d)(\d)?(\d)?\]/g, ($1, $2, $3, $4, $5)=>{
           console.log($3)
           
           
