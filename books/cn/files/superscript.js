0ab783b6e98e9d62ee571b9b4d9bbe357b39b887
tanlooger <tanlooger@gmail.com>
2023-07-24 09:28:14 +0800

    金圣叹评点才子古文

--- books/cn/files/superscript.js
+++ books/cn/files/superscript.js
@@ -8,7 +8,7 @@ const ss = ['⁰','¹','²','³','⁴','⁵','⁶','⁷','⁸','⁹']
 // find superscript
 // \p{No}
 
-const filename = '../251/a.txt'
+const filename = '../283/a.txt'
 
     fs.readFile(filename, 'utf8', function (err,data) {
         if (err) {
@@ -19,6 +19,7 @@ const filename = '../251/a.txt'
 
         var reg = /\[(\d)(\d)?(\d)?\]/g
         reg = /(\d)(\d)?(\d)?/g
+        reg = /〔(\d)(\d)?(\d)?〕/g
         result = data.replace(reg, ($1, $2, $3, $4, $5)=>{
           console.log($3)
           
