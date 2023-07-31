e99e982cb8e06db6f56da28ffb9b85d848e991c8
tanlooger <tanlooger@gmail.com>
2023-07-31 08:10:36 +0800

    从惊奇开始：青少年哲学第一课 - 刘擎等著

--- books/cn/files/make.js
+++ books/cn/files/make.js
@@ -50,7 +50,7 @@ const data = fs.readFileSync('../150/'+'a.txt', 'utf8');
 
 console.log('adsfsd '+myArray.length)
 
-for(let i=1; i<=17; i++){
+for(let i=301; i<=330; i++){
   //for(let i=1; i<=myArray.length; i++){
     //console.log(i)
   //const chapter = myArray[i].split("adfdsdeo97j")
