5c887b88090d48b47f578d2fb372616827671411
tanlooger <tanlooger@gmail.com>
2023-08-12 21:07:28 +0800

    霍乱时期的爱情(马尔克斯) - 杨玲译

--- books/cn/files/chapters.js
+++ books/cn/files/chapters.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
 //const bookid = process.argv[2];
-const bookid = 314
+const bookid = 322
 let bookdata = fs.readFileSync('../'+bookid+'/a.txt', 'utf8')
 //bookdata = bookdata.replace(/\r\n/g,'\n')
 if(/\r/.test(bookdata)){
@@ -51,6 +51,7 @@ reg = /\n(第[一二三四五六七八九十百零]{1,5}章 .*|[0-9]{1,5} .*)\n/
 reg = /\n(([一二三四五六七八九十]{1,3} .*)|([0-9]{1,5}))\n/g
 reg = /\n(附录[一二三四五].*|第[一二三四五六七八九十百零]{1,5}章.*|[一二三四五六七八九十〇]{1,3}、.*)\n/g
 reg = /\n(第.{1,3}回 .* .*)\n/g
+reg = /\n(第[一二三四五六七八九十百零]{1,5}章|[一二三四五六七八九十〇]{1,3})\n/g
 
 
 
