f4a68871126d0d1b5876d8bcd6ac7d2c9d87060b
tanlooger <tanlooger@gmail.com>
2023-09-23 14:26:25 +0800

    聊斋志异 - 蒲松龄、于天池、孙通海、于天池

--- books/cn/files/superscript.js
+++ books/cn/files/superscript.js
@@ -8,7 +8,7 @@ const ss = ['⁰','¹','²','³','⁴','⁵','⁶','⁷','⁸','⁹']
 // find superscript
 // \p{No}
 
-const filename = '../61/a.txt'
+const filename = '../6/a.txt'
 
     fs.readFile(filename, 'utf8', function (err,data) {
         if (err) {
@@ -20,8 +20,8 @@ const filename = '../61/a.txt'
         var reg = /\[(\d)(\d)?(\d)?\]/g
         reg = /(\d)(\d)?(\d)?/g
         reg = /〔(\d)(\d)?(\d)?〕/g
-        reg = /\[(\d)(\d)?(\d)?\]/g
         reg = /\((\d)(\d)?(\d)?\)/g
+        reg = /\[(\d)(\d)?(\d)?\]/g
 
         result = data.replace(reg, ($1, $2, $3, $4, $5)=>{
           console.log($3)
